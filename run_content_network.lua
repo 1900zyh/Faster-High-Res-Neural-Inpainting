9cad0caacb9d129bd63ef7074d8485e8cef55ce3
1900zyh <zengyh7@mail2.sysu.edu.cn>
2019-10-16 23:12:28 +0800

    debug:-

--- run_content_network.lua
+++ run_content_network.lua
@@ -26,27 +26,30 @@ end
 modelG=util.load(opt.model_file,opt.gpu)
 modelG:evaluate()
 
-local real=torch.Tensor(32,3,512,512)
-local real_ctx = torch.Tensor(32,3,128,128)
-local fake2 = torch.Tensor(3,256,256)
-local output=torch.Tensor(3,512,512)
+fnames = paths.dir('examples')
+fnames = { unpack(fnames, 3, #fnames)}
+print(string.format('testing %d images ...', #fnames))
 
-for i=1,32 do
-    real[i]=loadImage(string.format('examples/input_%04d.png',i),512)
-    real_ctx[i]:copy(image.scale(real[i],128,128))
-    real_ctx[{i,{1},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*117/255.0 - 1.0
-    real_ctx[{i,{2},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*104/255.0 - 1.0
-    real_ctx[{i,{3},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*123/255.0 - 1.0
-end
-fake = modelG:forward(real_ctx)
-for i=1,32 do
-    fake2:copy(image.scale(fake[i],256,256))
-    output:copy(real[i])
-    output[{{},{145,368},{145,368}}]:copy(fake2[{{},{17, 240},{17, 240}}])
-    output[output:gt(1)]=1
-    output[output:lt(-1)]=-1
-    output=(output+1)/2
-    image.save(string.format('examples/fake_%04d.png',i),output)
+for i=1,#fnames do
+
+  local real=torch.Tensor(1,3,512,512)
+  local real_ctx = torch.Tensor(1,3,128,128)
+  local fake2 = torch.Tensor(3,256,256)
+  local output=torch.Tensor(3,512,512)
+  real[0]=loadImage(string.format('examples/%s',fnames[i]),512)
+  real_ctx[0]:copy(image.scale(real[i],128,128))
+  real_ctx[{0,{1},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*117/255.0 - 1.0
+  real_ctx[{0,{2},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*104/255.0 - 1.0
+  real_ctx[{0,{3},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred},{1 + opt.fineSize/4 + opt.overlapPred, opt.fineSize/2 + opt.fineSize/4 - opt.overlapPred}}] = 2*123/255.0 - 1.0
+  fake = modelG:forward(real_ctx)
+
+  fake2:copy(image.scale(fake[0],256,256))
+  output:copy(real[0])
+  output[{{},{145,368},{145,368}}]:copy(fake2[{{},{17, 240},{17, 240}}])
+  output[output:gt(1)]=1
+  output[output:lt(-1)]=-1
+  output=(output+1)/2
+  image.save(string.format('examples/fake_%s',fnames[i]),output)
 end
 
 
