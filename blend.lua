9555aa8ae5837329d4acabde13056e2ff21c42d8
1900zyh <zengyh7@mail2.sysu.edu.cn>
2019-10-17 11:26:59 +0900

    debug:blend

--- blend.lua
+++ blend.lua
@@ -6,11 +6,15 @@ fake_fnames = {}
 for i=1,#all_fnames do 
   if string.match(all_fnames[i], "demo") then
     table.insert(demo_fnames, all_fnames[i])
-  end
-  if string.match(all_fnames[i], "fake") then
-    table.insert(fake_fnames, all_fnames[i])
+  else
+    if string.match(all_fnames[i], "fake") then
+      table.insert(fake_fnames, all_fnames[i])
+    end
   end
 end
+
+table.sort(fake_fnames)
+table.sort(demo_fnames)
 
 for i=1,#demo_fnames do
     src=image.load(string.format('examples/%s',string.sub(fake_fnames[i], 6, #fake_fnames[i])))
@@ -18,5 +22,5 @@ for i=1,#demo_fnames do
     mask=image.load('mask.png')
     mask=mask[{{1,3},{},{}}]
     res=torch.cmul(src,mask)+torch.cmul(tgt,1-mask)
-    image.save(string.format('examples/result_%s.png',i),res)
+    image.save(string.format('examples/result_%s.png',string.sub(fake_fnames[i], 6, #fake_fnames[i]-4)),res)
 end
